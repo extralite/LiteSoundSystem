5ca0e2b225a439465ce36f96f22a35d308d0bf95
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-02-21 20:41:58 -0300

    Find packages utility

---
+++ cmake/FindOgg.cmake
@@ -0,0 +1,5 @@
+set(OGG_DIR ${CMAKE_SOURCE_DIR}/ogg)
+set(OGG_INCLUDE_DIR ${OGG_DIR}/include ${CMAKE_BINARY_DIR}/ogg/include)
+
+set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+add_subdirectory(${OGG_DIR} ${CMAKE_BINARY_DIR}/ogg)
---
+++ cmake/FindOpus.cmake
@@ -0,0 +1,5 @@
+set(OPUS_DIR ${CMAKE_SOURCE_DIR}/opus)
+set(OPUS_INCLUDE_DIR ${OPUS_DIR}/include)
+
+set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+add_subdirectory(${OPUS_DIR} ${CMAKE_BINARY_DIR}/opus)
