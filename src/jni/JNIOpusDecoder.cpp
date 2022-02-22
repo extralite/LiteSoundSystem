6c6c1673d109763c029adf876a4e3dd2ce215464
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-02-22 20:31:12 -0300

    JNI implementation of OpusStreamDecoder

---
+++ src/jni/JNIOpusDecoder.cpp
@@ -0,0 +1,45 @@
+#include "../OpusStreamDecoder.hpp"
+
+#include <jni.h>
+
+#include <iostream>
+#include <fstream>
+
+extern "C" {
+
+JNIEXPORT jlong JNICALL Java_io_github_ve_soundsystem_OpusDecoder_ncreate(JNIEnv *env, jclass clazz, jstring file) {
+    const char *fileName = env->GetStringUTFChars(file, nullptr);
+    std::unique_ptr<std::istream> stream(
+            new std::ifstream(fileName, std::ios::in | std::ios::binary)
+    );
+
+    auto *decoder = new OpusStreamDecoder(std::move(stream));
+
+    return reinterpret_cast<intptr_t>(decoder);
+}
+
+JNIEXPORT jshortArray JNICALL Java_io_github_ve_soundsystem_OpusDecoder_nread(JNIEnv *env, jclass clazz, jlong _decoder) {
+    auto *decoder = reinterpret_cast<OpusStreamDecoder *>(_decoder);
+
+    opus_int16 pcm[OPUS_MAX_FRAME_SAMPLE_INTERLEAVED];
+
+    int ret = decoder->read(pcm, OPUS_MAX_FRAME_SAMPLE_INTERLEAVED);
+
+    if (ret > 0) {
+        int size = ret * 2;
+        jshortArray arr = env->NewShortArray(size);
+
+        env->SetShortArrayRegion(arr, 0, size, pcm);
+
+        return arr;
+    }
+
+    return nullptr;
+}
+
+JNIEXPORT void JNICALL Java_io_github_ve_soundsystem_OpusDecoder_nfree(JNIEnv *env, jclass clazz, jlong _decoder) {
+    auto *decoder = reinterpret_cast<OpusStreamDecoder *>(_decoder);
+    delete decoder;
+}
+
+}
